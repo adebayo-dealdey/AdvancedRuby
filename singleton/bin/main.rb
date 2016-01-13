4b88515d62265d58f582306dc3625e8208192a1b
Akinlaja Adebayo <adebayo@dealdey.com>
2016-01-13 17:25:44 +0100

    Singleton Exercise Completed

---
+++ singleton/bin/main.rb
@@ -0,0 +1,16 @@
+string_instance = String.new('I am an instance of a string class. That is what I really am. Hey! Lucky. I am.')
+# First Method
+def string_instance.split_by_spaces
+  split(/\s+/)
+end
+p string_instance.split_by_spaces
+
+# Second Method
+string_instance.instance_eval do
+  class << self
+    def split_by_dots
+      split('.')
+    end
+  end
+end
+p string_instance.split_by_dots
